f80ce17d7217725c5c6289848d5d7ccf81cc6d71
mibarrios <miguelbarrios001@gmail.com>
2026-02-16 18:47:02 -0500

    probando actualizar el repo github con cambios del local

--- hellogit.py
+++ hellogit.py
@@ -1,1 +1,2 @@
 print('he cambiado el print, otra vez')
+print('para actualizar repo en github a partir de trabajo local')
--- hellogit2.py
+++ hellogit2.py
@@ -1,1 +1,2 @@
 print('Hello git 2!')
+print('Otra linea para probar cómo actualizo de local a github')
